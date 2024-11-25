db1b13a25a15ff2654d8b633422fa4f3eee3e437
Starshix <160054944+Starshix@users.noreply.github.com>
2024-11-25 23:40:11 +0300

    Add files via upload

---
+++ main.cmd
@@ -0,0 +1,7 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'main.exe' with Python DLL being found.
+set PATH=c:\users\user\appdata\local\programs\python\python312;%PATH%
+set PYTHONHOME=C:\Users\User\AppData\Local\Programs\Python\Python312
+set NUITKA_PYTHONPATH=C:\Users\User\PycharmProjects\pythonProject;C:\Users\User\AppData\Local\Programs\Python\Python312\DLLs;C:\Users\User\AppData\Local\Programs\Python\Python312\Lib;C:\Users\User\AppData\Local\Programs\Python\Python312;C:\Users\User\PycharmProjects\pythonProject\venv;C:\Users\User\PycharmProjects\pythonProject\venv\Lib\site-packages
+"%~dp0main.exe" %*
